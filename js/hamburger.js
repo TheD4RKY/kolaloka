1445ac97bca914ae07a020a50bd2a4584a64f794
TheD4RKY <growlbox123g@gmail.com>
2022-08-25 16:27:38 +0200

    functionality and interactivity to navbar

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  var y = document.getElementById("translatetoggle");
+  y.classList.toggle("active");
+  y.classList.toggle("menu_overlay");
+}
